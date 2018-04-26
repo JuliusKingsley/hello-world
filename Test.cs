158d8bae8c29e1b2099b6f04a95c9e7be8239c4d
JuliusKingsley <837800998@qq.com>
2018-04-26 16:26:19 +0800

    1

---
+++ Test.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Xml;
+
+public class Test
+{
+  public string PropertyName1 { get; set; }
+  public string PropertyName2 { get; set; }
+  public void Set(){
+    XmlDocument x=new XmlDocument();
+  }
+}
